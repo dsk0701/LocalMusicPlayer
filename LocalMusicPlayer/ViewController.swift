7fe5df993602953e6844f626bae43628888197d3
dsk0701 <d.dubandtribal@gmail.com>
2017-12-26 10:03:23 +0900

    Show album list.

---
+++ LocalMusicPlayer/ViewController.swift
@@ -0,0 +1,43 @@
+import UIKit
+import MediaPlayer
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+    private let albums: [MPMediaItemCollection] = {
+        guard let albums = MPMediaQuery.albums().collections else { return [MPMediaItemCollection]() }
+        return albums
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return albums.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let album = albums[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = album.representativeItem?.albumTitle
+        cell.detailTextLabel?.text = album.representativeItem?.albumArtist
+        if let artwork = album.representativeItem?.artwork {
+            cell.imageView?.image = artwork.image(at: artwork.bounds.size)
+        } else {
+            cell.imageView?.image = nil
+        }
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
