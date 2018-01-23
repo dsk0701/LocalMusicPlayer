01741b99e23ae68ae5ee332a4191fe9770fd8d35
dsk0701 <d.dubandtribal@gmail.com>
2018-01-23 11:43:09 +0900

    Try stack view.

--- LocalMusicPlayer/Sources/TopViewController.swift
+++ LocalMusicPlayer/Sources/TopViewController.swift
@@ -2,17 +2,46 @@ import UIKit
 
 class TopViewController: UIViewController {
 
+    @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var footerView: UIView!
+    lazy var playerView: UIButton = {
+        let view = UIButton()
+        view.backgroundColor = .purple
+        view.addTarget(self, action: #selector(btnDidTap), for: .touchUpInside)
+        return view
+    }()
 
+    @objc func btnDidTap() {
+        UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut], animations: {
+            self.footerView.isHidden = !self.footerView.isHidden
+        })
+    }
+
+    /*
     private let menus = [
         R.string.localizable.topMenuAlbum(),
         R.string.localizable.topMenuTrack(),
         R.string.localizable.topMenuArtist(),
         R.string.localizable.topMenuPlaylist(),
     ]
+    */
+
+    private let menus = (0..<100).map { String($0) }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view.addSubview(playerView)
+        // stackView.addArrangedSubview(playerView)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        let size = min(view.frame.width, view.frame.height)/4
+        playerView.frame = CGRect(x: view.frame.width - size - 8, y: view.frame.height - size - 8, width: size, height: size)
+        playerView.layer.cornerRadius = size/2
     }
 }
 
