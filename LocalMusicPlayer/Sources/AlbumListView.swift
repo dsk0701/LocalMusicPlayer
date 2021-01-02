9780a8e5effad4eb4bc29bc98729f430afbf9572
dsk0701 <d.dubandtribal@gmail.com>
2021-01-02 10:43:27 +0900

    format

--- LocalMusicPlayer/Sources/AlbumListView.swift
+++ LocalMusicPlayer/Sources/AlbumListView.swift
@@ -6,20 +6,20 @@ struct AlbumListView: View {
 
     var body: some View {
         NavigationView {
-             ScrollView {
-                 // 表示されているものだけ読み込むためにLazyStack.
-                 LazyVStack(alignment: .leading) {
-                     ForEach(albums) { (album) in
-                         NavigationLink(destination: AlbumDetailView(album: album)) {
-                             AlbumListRow(album: album)
-                         }
-                     }
-                 }
-                 // contentInsetをつけている。
-                 // このやり方の記事が多く書かれているが、どうしても少し余分な高さが出てしまうのでRectangleを使うようにした。
-                 // Color.clear.padding(.bottom, playerViewHeight)
-                 Rectangle().fill(Color.clear).frame(height: playerViewHeight)
-             }
+            ScrollView {
+                // 表示されているものだけ読み込むためにLazyStack.
+                LazyVStack(alignment: .leading) {
+                    ForEach(albums) { (album) in
+                        NavigationLink(destination: AlbumDetailView(album: album)) {
+                            AlbumListRow(album: album)
+                        }
+                    }
+                }
+                // contentInsetをつけている。
+                // このやり方の記事が多く書かれているが、どうしても少し余分な高さが出てしまうのでRectangleを使うようにした。
+                // Color.clear.padding(.bottom, playerViewHeight)
+                Rectangle().fill(Color.clear).frame(height: playerViewHeight)
+            }
                 .navigationTitle(Text("アルバム"))
         }
     }
