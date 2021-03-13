3fb47e4d4b8681214d25d4777bec5823eca41733
dsk0701 <d.dubandtribal@gmail.com>
2021-03-13 17:07:52 +0900

    Remove unnecessary codes.

--- LocalMusicPlayer/Sources/FullScreenPlayerView.swift
+++ LocalMusicPlayer/Sources/FullScreenPlayerView.swift
@@ -3,7 +3,6 @@ import SwiftUI
 struct FullScreenPlayerView: View {
     @Environment(\.presentationMode) var presentationMode
     @GestureState private var dragOffset = CGSize.zero
-    @State private var position = CGSize.zero
     private let dissmissThreshold: CGFloat = 100.0
 
     var body: some View {
@@ -13,7 +12,7 @@ struct FullScreenPlayerView: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.red)
         .edgesIgnoringSafeArea(.all)
-        .offset(x: 0, y: dragOffset.height + position.height)
+        .offset(x: 0, y: dragOffset.height)
         .animation(.easeOut)
         .gesture(
             DragGesture()
@@ -24,7 +23,6 @@ struct FullScreenPlayerView: View {
                 })
                 .onEnded({ (value) in
                     if value.translation.height > dissmissThreshold {
-                        // position.height += value.translation.height
                         presentationMode.wrappedValue.dismiss()
                     }
                     print("value.translation.height: \(value.translation.height)")
